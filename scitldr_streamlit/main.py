137b128a9e2968e50362df12ecf76b239b27207a
sobamchan <oh.sore.sore.soutarou@gmail.com>
2023-02-23 16:53:56 +0100

    use schnitsum

--- scitldr_streamlit/main.py
+++ scitldr_streamlit/main.py
@@ -1,33 +1,5 @@
 import streamlit as st
-from transformers import (AutoTokenizer, BartConfig,
-                          BartForConditionalGeneration)
-import torch
-
-
-class Model:
-    def __init__(self, model_path):
-        print("initializing...")
-        self.tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
-        bart = BartForConditionalGeneration(BartConfig())
-        bart.load_state_dict(
-            torch.load(model_path),
-            strict=False,
-        )
-        self.bart = bart
-        print("loaded!")
-
-    def summarize(self, text: str):
-
-        inputs = self.tokenizer(
-            [text], padding="max_length", truncation=True, return_tensors="pt"
-        )
-        summary_ids = self.bart.generate(
-            inputs["input_ids"],
-            max_length=50,
-            num_beams=1,
-            early_stopping=True,
-        )
-        return self.tokenizer.batch_decode(summary_ids, skip_special_tokens=True)[0]
+from schnitsum import SchnitSum
 
 
 st.title("SCITLDR demo.")
@@ -38,6 +10,6 @@ txt = st.text_area(
      """,
 )
 
-model = Model("./model.state")
+model = SchnitSum("sobamchan/bart-large-scitldr")
 st.subheader("TLDR: ")
-st.write(model.summarize(txt))
+st.write(model([txt])[0])
